0fb4b30967d804a1439f2a312f1cdf73b393a71f
niraj.meshram <niraj.meshram@cgi.com>
2025-12-24 23:59:59 -0700

    Add restaurant name generator

---
+++ RestuarantNameGenerator/langchain_helper.py
@@ -0,0 +1,5 @@
+def generate_restaurant_name_and_items(cuisine):
+    return {
+        'restaurant_name' : 'Curry Delight',
+        'menu_items' :  'Butter Chicken, Chicken Biryani, Paneer Tikka, Chole Bhature, Masala Dosa'
+    }
